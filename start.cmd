6490a499d781b605a4c4b553804829275b9643f5
uik <oplata@jap-market.ru>
2022-01-26 17:50:11 +0300

    2022.1.26 17:50:10

--- start.cmd
+++ start.cmd
@@ -2,9 +2,9 @@ echo on
 for /f %%# in ('wMIC Path Win32_LocalTime Get /Format:value') do @for /f %%@ in ("%%#") do @set %%@
 rem set d='date /t'
 rem set t='time /t'
-copy D:\UIK\Exchange_AliExpress_\webdata\* D:\UIK\Exchange_AliExpress\webdata
-d:
-cd \UIK\Exchange_AliExpress\webdata
+copy E:\Exchange_AliExpress_\webdata\* E:\Exchange_AliExpress\webdata
+e:
+cd \Exchange_AliExpress\webdata
 git status
 git add .
 git commit -a -m "%year%.%month%.%day% %hour%:%minute%:%second%"
